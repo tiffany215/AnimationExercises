75d65a61bc68a068e51334e0c4f8bd59e9592195
wangxu <284993100@qq.com>
2019-03-13 00:42:15 +0800

    1:添加补间动画

--- tweenAnimation/src/main/java/com/example/tweenanimation/MainActivity.java
+++ tweenAnimation/src/main/java/com/example/tweenanimation/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.tweenanimation;
 
+import android.service.autofill.TextValueSanitizer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Animation mAnimation;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = findViewById(R.id.text_content);
+
+        mAnimation = AnimationUtils.loadAnimation(this,R.anim.scale_animtion);
+    }
+
+    public void startAnimation(View view) {
+        mTextView.startAnimation(mAnimation);
+    }
+
+    public void cancelAnimation(View view) {
+        mTextView.clearAnimation();
     }
 }
